7c288f7288195af9477eba6cf7ac459c84fb434a
unknown <nasir-1996@hotmail.co.uk>
2014-09-10 23:10:33 +0100

    Cleaned up previous source

--- app/src/main/java/is/glitch/innaapp/activities/LoginActivity.java
+++ app/src/main/java/is/glitch/innaapp/activities/LoginActivity.java
@@ -1,17 +1,9 @@
 package is.glitch.innaapp.activities;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.EditText;
 
 import is.glitch.innaapp.R;
 import is.glitch.innaapp.fragments.LoginFragment;
@@ -21,6 +13,7 @@ public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
@@ -32,28 +25,19 @@ public class LoginActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.login, menu);
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
         if (id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    public void Login(View view)
-    {
-        EditText user = (EditText) findViewById(R.id.username_input);
-        String name = user.getText().toString();
-        Log.v("Inna", name);
     }
 
 }
--- app/src/main/java/is/glitch/innaapp/fragments/LoginFragment.java
+++ app/src/main/java/is/glitch/innaapp/fragments/LoginFragment.java
@@ -2,9 +2,11 @@ package is.glitch.innaapp.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import is.glitch.innaapp.R;
 
@@ -13,14 +15,25 @@ import is.glitch.innaapp.R;
  */
 public class LoginFragment extends Fragment {
 
+	private View view = null;
+	private EditText usernameInput = null;
+
 	public LoginFragment() {
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_login, container, false);
-		return rootView;
+		view = inflater.inflate(R.layout.fragment_login, container, false);
+
+		usernameInput = (EditText) view.findViewById(R.id.input_username);
+
+		return view;
 	}
 
+
+	public void Login() {
+		String usernameTxt = usernameInput.getText().toString();
+		Log.v("Inna", usernameTxt);
+	}
 }
